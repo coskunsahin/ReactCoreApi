06def2f46461a503c281cb247bf24d3816575813
coskunsahin <coskunsahin1981@yahoo.com>
2020-05-01 01:31:19 +0300

    order list

---
+++ ReactCoreWebApi/ReactCoreWebApi/Controllers/OrderController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReactCoreWebApi.Models;
+
+namespace ReactCoreWebApi.Controllers
+{
+    [EnableCors("_myAllowSpecificOrigins")]
+    [ApiController]
+    [Produces("application/json")]
+    [Route("api/Order")]
+
+    public class OrderController : ControllerBase
+    {
+        private NorthwindContext db = new NorthwindContext();
+       
+           
+
+            [HttpGet("SHOW")]
+            public async Task<IActionResult> FindAll()
+            {
+                try
+                {
+                    var orders = await db.Orders.Select(p => new
+                    {
+                        orderID = p.OrderId,
+                        ememployeeId = p.EmployeeId,
+                        orderDate = p.OrderDate,
+                        requiredDate = p.RequiredDate,
+                        shipPostalCode=p.ShipPostalCode
+
+
+
+                    }).ToListAsync();
+                    return Ok(orders);
+                }
+                catch
+                {
+                    return BadRequest();
+                }
+
+            }
+        [Produces("application/json")]
+        [HttpGet("search")]
+            public async Task<ActionResult<Orders>> search(DateTime min, DateTime max)
+        {
+            var orders = await db.Orders.Where(p => p.OrderDate >= min && p.OrderDate <= max)
+            //    .Select(p => new
+            //{
+            //    orderID = p.OrderId,
+            //    ememployeeId = p.EmployeeId,
+            //    orderDate = p.OrderDate,
+            //    requiredDate = p.RequiredDate,
+            //    shipPostalCode = p.ShipPostalCode
+
+
+
+            //})
+
+           
+            .ToListAsync();
+            return Ok(orders);
+        }
+        }
+
+    }
